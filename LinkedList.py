0dd11eba087b0fed005aaa662603fa5de5a7692d
loucq123 <loucq123@gmail.com>
2015-03-14 21:56:09 +0800

    change the needed

--- LinkedList.py
+++ LinkedList.py
@@ -16,7 +16,7 @@ class LinkedList():
     def is_empty(self):
         return self.front == None
 
-    def insert(self, item):
+    def insert_after(self, item):
         insertItem = Node(item, None)
         if self.is_empty():
             self.front = insertItem
@@ -24,6 +24,22 @@ class LinkedList():
         else:
             self.rear.next = insertItem
             self.rear = insertItem
+
+    def insert_before(self, item):
+        insertItem = Node(item, None)
+        if self.is_empty():
+            self.front = insertItem
+            self.rear = insertItem
+        else:
+            insertItem.next = self.front
+            self.front = insertItem
+    def find(self, item):
+        first = self.front
+        while first != None:
+            if first.element is item:
+                return first
+            first = first.next
+        return None
 
     def delete(self):
         if self.is_empty():
@@ -62,44 +78,59 @@ def testNodeCreation():
     assert n.element == 10
 
 
-def testLinkedListInsertCreation():
+def testLinkedListInsert_afterCreation():
     testList = LinkedList()
-    testList.insert(1)
+    testList.insert_after(1)
     assert testList.first_node().element == 1 and testList.last_node().element == 1
-    testList.insert(2)
+    testList.insert_after(2)
     assert testList.first_node().element == 1 and testList.last_node().element == 2
 
+def testLinkedListInsert_beforeCreation():
+    testList = LinkedList()
+    testList.insert_before(1)
+    assert testList.first_node().element == 1 and testList.last_node().element == 1
+    testList.insert_before(2)
+    assert testList.first_node().element == 2 and testList.last_node().element == 1
+
+
+def testLinkedListFindCreation():
+    testList = LinkedList()
+    testList.insert_after(1)
+    testList.insert_after(2)
+    assert testList.find(2) == testList.rear and testList.find(3) == None
 
 def testLinkedListCountCreation():
     testList = LinkedList()
     assert testList.count() == 0
-    testList.insert(111)
+    testList.insert_after(111)
     assert testList.count() == 1
 
 
 def testLinkedListDeleteCreation():
     testList = LinkedList()
-    testList.insert(1)
-    testList.insert(2)
+    testList.insert_after(1)
+    testList.insert_after(2)
     testList.delete()
     assert testList.first_node().element == 1 and testList.last_node().element == 1
 
 
 def testLinkedListFirst_nodeCreation():
     testList = LinkedList()
-    testList.insert(1)
+    testList.insert_after(1)
     assert testList.first_node() == testList.front
 
 
 def testLinkedListLast_nodeCreation():
     testList = LinkedList()
-    testList.insert(1)
-    testList.insert(2)
+    testList.insert_after(1)
+    testList.insert_after(2)
     assert testList.last_node() == testList.rear
 
 
 def testLinkedListCreation():
-    testLinkedListInsertCreation()
+    testLinkedListInsert_afterCreation()
+    testLinkedListInsert_beforeCreation()
+    testLinkedListFindCreation()
     testLinkedListCountCreation()
     testLinkedListDeleteCreation()
     testLinkedListFirst_nodeCreation()
